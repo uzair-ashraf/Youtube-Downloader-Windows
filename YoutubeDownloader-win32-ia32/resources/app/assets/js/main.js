0c243c083d892aeb4f4ff140dd10d13ea47beb60
Uzair Ashraf <uzinatorcl@live.com>
2020-02-24 22:03:57 -0800

    production release v1.0.0

---
+++ YoutubeDownloader-win32-ia32/resources/app/assets/js/main.js
@@ -0,0 +1,112 @@
+const { ipcRenderer } = require('electron');
+
+let selectedVideo = null;
+
+const searchForm = document.querySelector('.youtube-search-form');
+const thumbnailImage = document.querySelector('.thumbnail');
+const thumbnailText = document.querySelector('.thumbnail-text');
+const urlInput = document.querySelector('.url-input');
+const searchButton = document.querySelector('.search-button');
+const resetButton = document.querySelector('.reset-button');
+const loadingBarPercentage = document.querySelector('.loading-bar-percentage');
+const loadingBar = document.querySelector('.loading-bar');
+searchForm.addEventListener('submit', searchVideo);
+resetButton.addEventListener('click', reset);
+
+ipcRenderer.on('update-percentage', (event, arg) => {
+  updateProgress(arg)
+})
+
+ipcRenderer.on('download-completed', downloadCompleted);
+
+ipcRenderer.on('directory-not-selected', resetDownload);
+
+ipcRenderer.on('bad-video-link', badLink);
+
+ipcRenderer.on('good-video-link', displayThumbnail);
+
+function searchVideo (event) {
+  event.preventDefault();
+  setButtonToLoadingMode(searchButton);
+  const formData = new FormData(event.target);
+  const url = formData.get('url');
+  ipcRenderer.send('get-video-info', url);
+}
+function badLink() {
+  thumbnailImage.src = "./assets/images/confused-logo-youtube.png";
+  thumbnailText.textContent = 'The link you provided was invalid, please try again.';
+  searchButton.disabled = false;
+  urlInput.disabled = false;
+  urlInput.value = '';
+  searchButton.textContent = 'Test Video';
+  searchButton.classList.remove('disabled');
+}
+function downloadVideo(event) {
+  event.preventDefault();
+  resetButton.classList.add('hidden');
+  setButtonToLoadingMode(searchButton);
+  ipcRenderer.send('download-video', (event, selectedVideo))
+}
+
+function displayThumbnail(event, response) {
+  selectedVideo = response;
+  urlInput.disabled = true;
+  const {title, thumbnail} = selectedVideo;
+  thumbnailImage.src = thumbnail;
+  thumbnailText.textContent = title;
+  switchMode();
+}
+function reset(event) {
+  event.preventDefault();
+  urlInput.disabled = false;
+  thumbnailImage.src = "./assets/images/logo-youtube.png";
+  thumbnailText.textContent = '';
+  loadingBarPercentage.textContent = '';
+  loadingBar.style.width = '0%';
+  selectedVideo = null;
+  searchButton.textContent = 'Test Video';
+  resetButton.classList.add('hidden');
+  resetButton.textContent = 'Reset'
+  searchButton.classList.remove('hidden');
+  searchForm.removeEventListener('submit', downloadVideo);
+  searchButton.disabled = false;
+  searchButton.classList.remove('disabled');
+  searchForm.addEventListener('submit', searchVideo);
+}
+
+function switchMode() {
+  searchForm.removeEventListener('submit', searchVideo);
+  searchForm.addEventListener('submit', downloadVideo);
+  searchButton.textContent = 'Download';
+  searchButton.disabled = false;
+  searchButton.classList.remove('disabled');
+  resetButton.classList.remove('hidden');
+}
+
+function setButtonToLoadingMode(element) {
+  const spinnerElement = document.createElement('i');
+  spinnerElement.classList.add('spinner');
+  element.textContent = '';
+  element.disabled = true;
+  element.classList.add('disabled')
+  element.append(spinnerElement);
+}
+
+function resetDownload() {
+  resetButton.classList.remove('hidden');
+  searchButton.textContent = 'Download';
+  searchButton.disabled = false;
+  searchButton.classList.remove('disabled');
+}
+
+function updateProgress(percentage) {
+  const percent = percentage + '%';
+  loadingBarPercentage.textContent = percent;
+  loadingBar.style.width = percent;
+}
+
+function downloadCompleted() {
+  resetButton.textContent = "Download Again?";
+  searchButton.classList.add('hidden');
+  resetButton.classList.remove('hidden');
+}
---
+++ YoutubeDownloader-win32-ia32/resources/app/index.js
@@ -0,0 +1,80 @@
+const electron = require('electron');
+const youtubedl = require('youtube-dl');
+const fs = require('fs');
+const template = require('./menu');
+const path = require('path');
+
+
+const { app, BrowserWindow, dialog, ipcMain, Menu, nativeImage } = electron;
+
+const menu = Menu.buildFromTemplate(template);
+
+Menu.setApplicationMenu(menu);
+
+let win;
+
+
+function createWindow () {
+    win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
+
+  win.setIcon(path.join(__dirname, '/assets/images/downloader-icon.ico'))
+
+  win.loadFile('index.html')
+}
+
+ipcMain.on('get-video-info', (event, args) => {
+  youtubedl.getInfo(args, null, (err, info) => {
+    if (err) {
+      win.webContents.send('bad-video-link', err);
+    } else {
+      win.webContents.send('good-video-link', info)
+    }
+  })
+})
+
+ipcMain.on('download-video', async (event, args) => {
+    try {
+      const result = await dialog.showOpenDialog(win, {
+        properties: ['openDirectory']
+      })
+      if(!result.filePaths.length) {
+        throw new Error('Please select a directory');
+      }
+      const [filePath] = result.filePaths;
+
+      const video = youtubedl(args.webpage_url,
+        ['--format=18'],
+        { cwd: __dirname })
+
+      // Will be called when the download starts.
+      let fileSize = 0;
+      video.on('info', info => {
+        fileSize = info.size;
+      })
+      // Getting download progress
+      let progress = 0;
+      video.on('data', chunk => {
+        progress += chunk.length;
+        if(fileSize) {
+        let percent = (progress / fileSize * 100).toFixed(2);
+        win.webContents.send('update-percentage', percent)
+        }
+      })
+      // Download completed
+      video.on('end', () => {
+        win.webContents.send('download-completed')
+      })
+      const downloadLocation = fs.createWriteStream(`${filePath}/${args._filename}`)
+      video.pipe(downloadLocation)
+    } catch (err) {
+      win.webContents.send('directory-not-selected');
+    }
+})
+
+app.on('ready', createWindow)
